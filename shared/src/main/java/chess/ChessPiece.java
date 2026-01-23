ba1aedf9b2fb7cab07dac5827e0a56ea8a8a3aac
Nathan <nathancbrady04@gmail.com>
2026-01-22 22:19:20 -0700

    phase 0, accidentally deleted part of ChessPiece and fixed it

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,7 @@
 package chess;
 
+import chess.PieceMoveCalculators.BishopMovesCalculator;
+
 import java.util.Collection;
 import java.util.List;
 
@@ -10,8 +12,12 @@ import java.util.List;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private final ChessGame.TeamColor pieceColor;
+    private final ChessPiece.PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +36,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return this.pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return this.type;
     }
 
     /**
@@ -48,6 +54,12 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            BishopMovesCalculator bishop = new BishopMovesCalculator();
+            return bishop.pieceMoves(board,myPosition);
+        }
+
         return List.of();
     }
 }
