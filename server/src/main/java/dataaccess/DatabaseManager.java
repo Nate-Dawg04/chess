76b96c9d0be2f2a967807a6bde861a646e6471e0
Nathan <nathancbrady04@gmail.com>
2026-03-11 15:28:12 -0600

    Phase 4, added unit tests for SQLUserDAO

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -1,7 +1,6 @@
 package dataaccess;
 
 import com.google.gson.Gson;
-import dataaccess.exceptions.DataAccessException;
 import dataaccess.exceptions.DatabaseException;
 
 import java.sql.*;
--- server/src/test/java/dataaccess/databaseDAOs/SQLUserDAOTest.java
+++ server/src/test/java/dataaccess/databaseDAOs/SQLUserDAOTest.java
@@ -1,7 +1,8 @@
 package dataaccess.databaseDAOs;
 
 import dataaccess.DatabaseManager;
-import dataaccess.exceptions.DataAccessException;
+import dataaccess.exceptions.DatabaseException;
+import model.UserData;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,46 +11,67 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQLUserDAOTest {
-    private static SQLUserDAO sqlUserDAO;
+    private SQLUserDAO sqlUserDAO;
 
     @BeforeAll
-    public static void createDatabase() {
-        try {
-            DatabaseManager.createDatabase();
-            sqlUserDAO = new SQLUserDAO();
-        } catch (Exception ex) {
-            System.out.println("Error creating the database for the tests");
-        }
-
+    public static void createDatabase() throws DatabaseException {
+        DatabaseManager.createDatabase();
     }
 
-//    @BeforeEach
-//    void setUp(){
-//
-//    }
+    @BeforeEach
+    void setUp() throws DatabaseException {
+        sqlUserDAO = new SQLUserDAO();
+    }
 
     @AfterEach
-    void tearDown() throws DataAccessException {
-
+    void tearDown() throws DatabaseException {
+        sqlUserDAO.deleteAllUsers();
     }
 
     @Test
-    void getUserPositive() {
+    void getUserPositive() throws DatabaseException {
+        // Tests getting a couple different users
+            UserData testUser = new UserData("Nathan","password","email");
+            sqlUserDAO.createUser(testUser);
+            UserData testUser2 = new UserData("John","other password","email2");
+            sqlUserDAO.createUser(testUser2);
+            assertEquals(testUser.username(),sqlUserDAO.getUser("Nathan").username());
+            assertEquals(testUser.email(),sqlUserDAO.getUser("Nathan").email());
+            assertEquals(testUser2.username(),sqlUserDAO.getUser("John").username());
+            assertEquals(testUser2.email(),sqlUserDAO.getUser("John").email());
     }
 
     @Test
-    void getUserNegative() {
+    void getUserNegative() throws DatabaseException {
+        // Tests that trying to get a user that doesn't exist returns null
+        assertNull(sqlUserDAO.getUser(""));
     }
 
     @Test
-    void createUserPositive() {
+    void createUserPositive() throws DatabaseException {
+        //Tests that creating a user inserts the user into the database
+        UserData testUser = new UserData("Test", "Super secret password", "email");
+        sqlUserDAO.createUser(testUser);
+        assertEquals(testUser.username(),sqlUserDAO.getUser("Test").username());
+        assertEquals(testUser.email(),sqlUserDAO.getUser("Test").email());
     }
 
     @Test
-    void createUserNegative() {
+    void createUserNegative() throws DatabaseException {
+        // Tests if trying to create a user which already exists throws a DatabaseException
+        UserData testUser = new UserData("Test", "Super secret password", "email");
+        UserData testUser2 = new UserData("Test", "Super secret password", "email");
+        sqlUserDAO.createUser(testUser);
+        assertThrows(DatabaseException.class, () -> sqlUserDAO.createUser(testUser2));
     }
 
     @Test
-    void deleteAllUsersPositive() {
+    void deleteAllUsersPositive() throws DatabaseException {
+        // Tests that the clear method works properly
+        sqlUserDAO.createUser(new UserData("Test1", "Test1","Test1"));
+        sqlUserDAO.createUser(new UserData("Test2", "Test2","Test2"));
+        sqlUserDAO.createUser(new UserData("Test3", "Test3","Test3"));
+        sqlUserDAO.deleteAllUsers();
+        assertNull(sqlUserDAO.getUser("Test3"));
     }
 }
