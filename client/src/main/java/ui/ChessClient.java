4c803a11e9ad67e8498060612302321c51001d32
Nathan <nathancbrady04@gmail.com>
2026-03-29 23:08:01 -0600

    Phase 5, restricts available commands based on login status, added logout functionality

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -2,6 +2,7 @@ package ui;
 
 import exception.ResponseException;
 import requests.LoginRequest;
+import requests.LogoutRequest;
 import requests.RegisterRequest;
 import server.ServerFacade;
 
@@ -13,6 +14,7 @@ import static ui.EscapeSequences.*;
 public class ChessClient {
     private final ServerFacade server;
     private State state = State.SIGNEDOUT;
+    private String authToken;
 
     public ChessClient(int port) throws ResponseException {
         server = new ServerFacade(port);
@@ -31,6 +33,7 @@ public class ChessClient {
             try {
                 result = eval(line);
                 System.out.print(SET_TEXT_COLOR_BLUE + result);
+                help();
             } catch (Throwable e) {
                 var msg = e.toString();
                 System.out.print(msg);
@@ -48,30 +51,35 @@ public class ChessClient {
             String[] tokens = input.toLowerCase().split(" ");
             String cmd = (tokens.length > 0) ? tokens[0] : "help";
             String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
-            return switch (cmd) {
-                case "login" -> login(params);
-                case "register" -> register(params);
-//                case "rescue" -> rescuePet(params);
-//                case "list" -> listPets();
-//                case "signout" -> signOut();
-//                case "adopt" -> adoptPet(params);
-//                case "adoptall" -> adoptAllPets();
-                case "quit" -> "quit";
-                default -> help();
-            };
+            if (state == State.SIGNEDOUT) {
+                return switch (cmd) {
+                    case "login" -> login(params);
+                    case "register" -> register(params);
+                    case "help" -> help();
+                    default -> invalidInput();
+                };
+            } else {
+                return switch (cmd) {
+                    case "quit" -> "quit";
+                    case "help" -> help();
+                    case "logout" -> logout();
+                    default -> invalidInput();
+                };
+            }
         } catch (ResponseException ex) {
             return ex.getMessage();
         }
     }
 
     public String invalidInput() {
-        System.out.print("\n" + SET_TEXT_COLOR_RED + "Please enter a valid command");
+        System.out.print("\n" + SET_TEXT_COLOR_RED + "Please enter a valid command" + "\n");
         return help();
     }
 
     public String help() {
         if (state == State.SIGNEDOUT) {
             return """
+                    - help
                     - login <username> <password>
                     - register <username> <password> <email>
                     - quit
@@ -79,11 +87,11 @@ public class ChessClient {
         }
         return """
                 - help
-                - logout
                 - createGame <gameName>
                 - listGames
                 - playGame <gameNumber> <WHITE|BLACK>
                 - observeGame <gameNumber>
+                - logout
                 - quit
                 """;
     }
@@ -92,6 +100,7 @@ public class ChessClient {
         if (params.length == 3) {
             try {
                 var registerResult = server.register(new RegisterRequest(params[0],params[1],params[2]));
+                authToken = registerResult.authToken();
                 state = State.SIGNEDIN;
                 return String.format("Welcome in %s!", registerResult.username());
             } catch (Exception ex) {
@@ -106,12 +115,25 @@ public class ChessClient {
             try {
                 var loginResult = server.login(new LoginRequest(params[0],params[1]));
                 state = State.SIGNEDIN;
+                authToken = loginResult.authToken();
                 return String.format("Welcome in %s!", loginResult.username());
             } catch (Exception ex) {
                 throw new ResponseException(ResponseException.Code.ClientError,ex.getMessage());
             }
         }
         throw new ResponseException(ResponseException.Code.ClientError, "Expected: <username> <password>");
+    }
+
+    public String logout() throws ResponseException {
+        try {
+            assertSignedIn();
+            // Need the authToken to be able to logout
+            server.logout(new LogoutRequest(authToken));
+            state = State.SIGNEDOUT;
+            return "Successfully logged out";
+        } catch (Exception ex) {
+            throw new ResponseException(ResponseException.Code.ClientError,ex.getMessage());
+        }
     }
 
     private void assertSignedIn() throws ResponseException {
